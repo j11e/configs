694e818923b0ad2a534e9856225b584461422c84
root <root@localhost.localdomain>
2015-10-07 19:33:46 +0200

    fixed link creation; still need to cd to script's directory

--- apply.sh
+++ apply.sh
@@ -2,10 +2,14 @@
 
 echo "copying config files and dotfiles..."
 
-ln -s ./vim/.vimrc ~/.vimrc
-ln -s ./tmux/.tmux.conf ~/.tmux.conf
-ln -s ./zsh/.zshrc ~/.zshrc
-ln -s ./Database.kdbx ~/Database.kdbx
+curdir=$(pwd)
+
+echo "applying config files from $curdir"
+
+ln -s $(echo $curdir)/vim/.vimrc ~/.vimrc
+ln -s $(echo $curdir)/tmux/.tmux.conf ~/.tmux.conf
+ln -s $(echo $curdir)/zsh/.zshrc ~/.zshrc
+ln -s $(echo $curdir)/Database.kdbx ~/Database.kdbx
 
 echo "done. Don't forget to correct path/homedir info."
 
