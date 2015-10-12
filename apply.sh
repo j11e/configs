466aecdfe4ec3f93a5462d8acdcf1f3fefe29c04
jd <jd@j11e.net>
2015-10-12 18:08:08 +0200

    added ssh config to apply.sh

--- apply.sh
+++ apply.sh
@@ -9,6 +9,7 @@ echo "applying config files from $curdir"
 ln -s $(echo $curdir)/vim/.vimrc ~/.vimrc
 ln -s $(echo $curdir)/tmux/.tmux.conf ~/.tmux.conf
 ln -s $(echo $curdir)/zsh/.zshrc ~/.zshrc
+ln -s $(echo $curdir)/ssh/config ~/.ssh/config
 ln -s $(echo $curdir)/Database.kdbx ~/Database.kdbx
 
 echo "done. Don't forget to correct path/homedir info."
