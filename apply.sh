8015bd7371f822f2f51a3f68b4060fa9d76c55c8
JD Innocenti <ajin@alphanetworks.be>
2017-02-17 14:38:04 +0100

    add .vim folder and update apply.sh to fix missing .ssh folder and copy aforementioned .vim folder

--- apply.sh
+++ apply.sh
@@ -7,8 +7,13 @@ curdir=${0:A:h}
 echo "applying config files from $curdir to homedir."
 
 ln -s $curdir/vim/.vimrc ~/.vimrc
+cp $curdir/vim/.vim ~/.vim -R
+
 ln -s $curdir/tmux/.tmux.conf ~/.tmux.conf
+
+mkdir ~/.ssh
 ln -s $curdir/zsh/.zshrc ~/.zshrc
+
 ln -s $curdir/ssh/config ~/.ssh/config
 ln -s $curdir/Database.kdbx ~/Database.kdbx
 
